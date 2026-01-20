3810448e40ba87016152b5b32efc8ff896a06e7d
Issy Smith <IssySmith@ibm.com>
2026-01-20 13:56:23 +0000

    string tests

--- test_palindrome.py
+++ test_palindrome.py
@@ -3,7 +3,12 @@ from main import is_palindrome
 def test_is_palindrome_true():
     assert is_palindrome(121) == True
     assert is_palindrome(12121) == True
+    assert is_palindrome("hannah") == True
+    assert is_palindrome("Hannah") == True
+    
     
 def test_is_palindrome_false():
     assert is_palindrome(122) == False
     assert is_palindrome(12122) == False
+    assert is_palindrome("steve") == False
+    assert is_palindrome("Steve") == False
